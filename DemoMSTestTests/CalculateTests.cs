cfab331887a6b8cf60f8156a5103b59bcd0b3894
Hank <aken1215@hotmail.com>
2016-10-14 23:27:27 +0800

    first commit

---
+++ DemoMSTestTests/CalculateTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestMsBuild;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMsBuild.Tests
+{
+    [TestClass()]
+    public class CalculateTests
+    {
+        [TestMethod()]
+        public void PlusTest()
+        {
+            var target = new Calculate();
+            var first = 1;
+            var second = 2;
+            var expected = 3;
+
+            var actual = target.Plus(first, second);
+
+            Assert.AreEqual(expected, actual);
+
+
+
+        }
+    }
+}
